df096b20cc47b32e0844c939a018cebc877113a4
Tyler G <guthriet1230@gmail.com>
2018-10-15 23:43:28 -0500

    functionality fixes

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -17,6 +17,7 @@ var config = {
   //////////////////////////////////////////////////////////////////
   //FIREBASE DATABASE DETAILS
   //////////////////////////////////////////////////////////////////
+  
   $("#submit").on("click", function(event) {
     event.preventDefault();
   
@@ -24,103 +25,67 @@ var config = {
     var destination = $("#destination").val().trim();
     var firstTrain = $("#first-train").val().trim();
     var frequency = $("#frequency").val().trim();
-  console.log(name, destination, firstTrain, frequency);
+    console.log(name, destination, firstTrain, frequency);
   
-  database.ref("/train").push({
+    database.ref("/train").push({
       name: name,
       destination: destination,
       train: firstTrain,
       frequency: frequency
-  })
-  });
+    });
   
   
-  
-  
-  
-  
-  var arrivalDetails = 3;
   
   //////////////////////////////////////////////////////////////////
   // REMOVE BUTTON
   //////////////////////////////////////////////////////////////////
   var removeButton = $("<button>Remove</button>").addClass("remove-button").val("remove");
+  // $(removeButton).on("click", function() {
   
-  
-  var minutesAway =3;
-  //////////////////////////////////////////////////////////////////////
-  //TIME
-  //////////////////////////////////////////////////////////////////////
-  
-  //////////////////////////////////////////////////////////////////////
-  //////////HELP
-  //////////////////////////////////////////////////////////////////////
-  // var tFrequency = 3;
-  // var firstTime = "12:30";
-  var tFrequency = $("#frequency").val().trim();
-  var firstTime = $("#first-train").val().trim();
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-  console.log(firstTimeConverted);
-  
-  var currentTime = moment();
-  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-  
-  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-  console.log("DIFFERENCE IN TIME: " + diffTime);
-  
-  var tRemainder = diffTime % tFrequency;
-  console.log(tRemainder);
-  
-  var tMinutesTillTrain = tFrequency - tRemainder;
-  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-  
-  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+  // });
   
   /////////////////////////////////////////////////////////////////
   // Set up the childe_added event for firebase employees location
   /////////////////////////////////////////////////////////////////
-  database.ref("/train").on("child_added", function(snapshot){
-      console.log(snapshot.val());
+  database.ref("/train").on("child_added", function(snapshot) {
+    console.log(snapshot.val());
   
-      var newRow = $("<div>").addClass("row");
-      var nameCol = $("<div>").addClass("col-2").text(snapshot.val().name);
-      var destCol = $("<div>").addClass("col-2").text(snapshot.val().destination);
-      var freqCol = $("<div>").addClass("col-2").text(snapshot.val().frequency);
-      //////////////////
-      // var startDate = moment(snapshot.val().startDate);
-      // console.log(startDate.format());
-      // console.log(moment().diff(startDate, "days"));
-      // /////////////////
-      var nextArrival = $("<div>").addClass("col-2").text(moment(nextTrain).format("hh:mm"));
-      ///////////////
+    var newRow = $("<div>").addClass("row");
+    var nameCol = $("<div>").addClass("col-2").text(snapshot.val().name);
+    var destCol = $("<div>").addClass("col-2").text(snapshot.val().destination);
+    var freqCol = $("<div>").addClass("col-2").text(snapshot.val().frequency);
+   
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
+    var firstTimeConverted = moment(frequency, "HH:mm").subtract(1, "years");
+    console.log("this is the firstTimeConverted " + firstTimeConverted);
   
-      ///////////////
-      var minAway = $("<div>").addClass("col-2").text(tMinutesTillTrain);
-      var remButton = $("<div>").addClass("col-2").append(removeButton);
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
   
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
   
-      newRow.append(nameCol, destCol, freqCol, nextArrival, minAway, remButton);
-      $("#new-train-rows").append(newRow)
-  })
+    var tRemainder = diffTime % frequency;
+    console.log(tRemainder);
+  
+    var tMinutesTillTrain = frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+  
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+  
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    var nextArrival = $("<div>").addClass("col-2").text(moment(nextTrain).format("hh:mm"));
+    var minAway = $("<div>").addClass("col-2").text(tMinutesTillTrain);
+    var remButton = $("<div>").addClass("col-2").append(removeButton);
+  
+    newRow.append(nameCol, destCol, freqCol, nextArrival, minAway, remButton);
+    $("#new-train-rows").append(newRow);
+  });
   
   ////////////////////////////////////////////////////
   //REMOVE BUTTON FUNCTION
   ////////////////////////////////////////////////////
-  $(removeButton).on("click", function(){
   
   });
